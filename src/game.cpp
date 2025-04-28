afe43452ef28c7c29753f40f0d21a42f8f2d7ea5
Stephen Hara <stephen.hara@stephenhara.com>
2025-04-28 17:41:53 +0900

    change a couple of logs

--- src/game.cpp
+++ src/game.cpp
@@ -3,17 +3,14 @@
 void game_update(SDL_Renderer *renderer, GameState *state) {
   SDL_FRect rect;
   float x, y;
-  SDL_Log("Game update - get mouse state start");
   auto mouseState = SDL_GetMouseState(&x, &y);
-  SDL_Log("Game update - get mouse state finish");
   rect.x = x;
   rect.y = y;
   rect.w = 100;
   rect.h = 200;
-  state->red += 1;
 
-  SDL_SetRenderDrawColor(renderer, state->red, 0, 0, 0);
-  SDL_RenderRect(renderer, &rect);
+  SDL_SetRenderDrawColor(renderer, 0, state->red, 0, 0);
+  SDL_RenderFillRect(renderer, &rect);
 }
 
 void game_init(GameState **state) {
